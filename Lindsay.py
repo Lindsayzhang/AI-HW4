7d4c2aacc04a370f01f0701a076798311a186f08
LindsayZ <lindsay.zwl@gmail.com>
2018-04-14 09:45:22 -0400

    First-time

---
+++ Lindsay.py
@@ -0,0 +1,17 @@
+TEAM_NAME = "mighty_ducks" #Pick a team name
+MEMBERS = ["mrf9n","jr5cf","kc4bf"]
+
+state = {
+	"team-code": "eef8976e",
+	"game": "sym",
+	"opponent-name": "mighty-ducks",
+	"prev-repetitions": 10, #Might be None if first game ever, or other number
+	"last-opponent-play": 1, #0 or 1 depending on strategy played
+	"last-outcome": 4, #Might be None if first game, or whatever outcome of play is
+	"prospects": [
+		[4,5],
+		[3,2]
+	]
+}
+
+def
