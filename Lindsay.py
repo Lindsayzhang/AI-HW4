ce0b6d0004e280fcc7e89c9739ec2bd0fe099424
LindsayZ <lindsay.zwl@gmail.com>
2018-04-14 09:47:15 -0400

    FirstTry

--- Lindsay.py
+++ Lindsay.py
@@ -14,4 +14,42 @@ state = {
 	]
 }
 
-def
+def decide(state):
+	if state["prospects"][0][0]<state["prospects"][1][0] and state["prospects"][0][1]>state["prospects"][1][1]:
+		cooperation = 0
+		betray = 1
+	else:
+		cooperation = 1
+		betray = 0	
+	if state["prev-repetitions"]==None:
+		history = {
+		"opponent-play":[],
+		"play":[betray]
+		}
+		return betray
+	elif state["prev-repetitions"]==1:
+		history = load_data()
+		history["opponent-play"].append(state["last-opponent-play"])
+		history["play"].append(cooperation)
+		return cooperation
+	else:
+		history = load_data()
+		history["opponent-play"].append(state["last-opponent-play"])
+		if (history["opponent-play"][-1]==betray and history["play"][-1]==cooperation):
+			history["play"].append(betray)
+			return betray
+		else:# history["opponent-play"][-1]==cooperation & both betray
+			history["play"].append(cooperation)
+			return cooperation
+
+def get_move(state):
+	if state["prospects"][0][0]>state["prospects"][1][0] and state["prospects"][0][1]>state["prospects"][1][1]:
+		final_move=0
+	elif state["prospects"][1][1]>state["prospects"][0][1] and state["prospects"][1][0]>state["prospects"][0][0]:
+		final_move=1
+	else:
+		final_move=decide(state)
+	return {
+	"team-code": state["team-code"]
+	"move": final_move
+	}
